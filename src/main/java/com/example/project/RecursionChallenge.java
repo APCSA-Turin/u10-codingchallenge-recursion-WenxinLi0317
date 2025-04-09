721765b3f7f10c39fac9320937edd6fdce6c9c9a
WenxinLi0317 <wenxinl8@nycstudents.net>
2025-04-09 12:40:28 +0000

    sda

--- src/main/java/com/example/project/RecursionChallenge.java
+++ src/main/java/com/example/project/RecursionChallenge.java
@@ -3,43 +3,107 @@
     public class RecursionChallenge {
 
             public static int bunnyEars(int n){
-                return 1;
+                if(n==1){
+                    return 2;
+                }else{
+                    return 2+ bunnyEars(n-1);
+                }
+                
             }
         
             public static int factorial(int n){
-                return 1;
+                if (n == 1){
+                    return 1;
+                }
+      
+                return n * factorial(n - 1);
             }
 
             public static int sumNumbers(int n){
-                return 1;
+                if(n==1){
+                    return 1;
+                }else{
+                    return n + sumNumbers(n-1);
+                }
+                
             }
 
             public static String countDown(int n){
-                return "";
+
+                if(n==0){
+                    return "Blast Off!";
+                }else{
+                    return "" + n + "," +countDown(n-1) + "," + " Blast Off!";
+                }
+              
             }
 
             public static int power(int x, int n){
-                return 1;
+                if(n==1){
+                    return x;
+                }
+                else{
+                    n--;
+                    return x*power(x, n);
+                }
+                
             }
 
             public static int fib(int n){
-                return 1;
+                if (n == 0 || n == 1)
+                {
+                    return n; 
+                }
+                return fib(n - 1) + fib(n - 2);
             }
 
             public static int countX(String s){
-                return 1;
+                if (s.length() <= 0)
+                {
+                    return 0;
+                }
+                if (s.substring(0, 1).equals("x"))
+                {
+                    return 1 + countX(s.substring(1));
+                }  
+                return countX(s.substring(1));
             }
 
             public static String changePi(String s){
-                return "";
+                if (s.length() <= 1)
+                {
+                    return s;
+                }
+
+                if (s.substring(0, 2).equals("pi"))
+                {
+                    return "3.14" + changePi(s.substring(2));
+                }
+
+                return s.substring(0, 1) + changePi(s.substring(1));
             }
 
 
             public static String reverse(String s){
-                return "";
+                if (s.length() <= 1)
+                {
+                    return s;
+                }
+
+                return s.substring(s.length() - 1) + reverse(s.substring(0, s.length() - 1));
             }
 
             public static Boolean isPalindrome(String s){
+                if (s.length() <= 1)
+                {
+                    return true;
+                }
+
+                if (s.charAt(0) == s.charAt(s.length() - 1))
+                {
+                    return isPalindrome(s.substring(1, s.length() - 1));
+                }
+
                 return false;
             }
 
---
+++ src/main/java/com/example/project/main.java
@@ -0,0 +1,9 @@
+package com.example.project;
+
+public class main {
+public static void main(String[] args) {
+    RecursionChallenge recur = new RecursionChallenge();
+    System.out.println(recur.power(2,3));
+}
+     
+}
